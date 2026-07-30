a9ecccdb41d289bd624fe57eb01b28623295d953
BertiniYves <79850117+BertiniYves@users.noreply.github.com>
2026-07-30 11:30:01 +0000

    Version définitive Webscraping Pierre Herme

---
+++ main.py
@@ -0,0 +1,53 @@
+# Scraping PIERREHERME.COM
+
+# *** ETAPE 0 à compléter ci-dessous PIERREHERME.COM ***
+
+
+
+# *** ETAPE 1 à compléter ci-dessous PIERREHERME.COM ***
+
+
+
+# *** ETAPE 2 à compléter ci-dessous PIERREHERME.COM ***
+
+
+
+# *** ETAPE 3 à compléter ci-dessous PIERREHERME.COM ***
+
+
+
+# *** ETAPE 4 à compléter ci-dessous PIERREHERME.COM ***
+
+
+
+# *** ETAPE 5 à compléter ci-dessous PIERREHERME.COM ***
+
+
+
+# *** ETAPE 6 à compléter ci-dessous PIERREHERME.COM ***
+
+
+
+# *** ETAPE 7 à compléter ci-dessous PIERREHERME.COM ***
+
+
+
+# *** ETAPE 8 à compléter ci-dessous PIERREHERME.COM ***
+
+
+
+# *** ETAPE 9 à compléter ci-dessous PIERREHERME.COM ***
+
+
+
+# *** ETAPE 10 à compléter ci-dessous PIERREHERME.COM ***
+
+
+
+# *** ETAPE 11 à compléter ci-dessous PIERREHERME.COM ***
+
+
+
+print('*** FIN PIERRE HERME ***')
+input('Presser Entrée pour arrêter')
+monpilote.close()
